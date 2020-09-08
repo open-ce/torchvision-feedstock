250992c6438c3edbf58543aaecb6d4a6a0fa2e1b
Bryant Nelson <bryant.nelson@ibm.com>
2020-09-08 18:32:27 -0500

    Initial copy.

---
+++ recipe/build.sh
@@ -0,0 +1,20 @@
+# *****************************************************************
+#
+# Licensed Materials - Property of IBM
+#
+# (C) Copyright IBM Corp. 2019, 2020. All Rights Reserved.
+#
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+# *****************************************************************
+set -ex
+
+FORCE_CUDA=0
+if [ $build_type == "cuda" ]
+then
+    export FORCE_CUDA=1
+    export NVCC_FLAGS="-ccbin $GXX"
+fi
+
+python setup.py install --single-version-externally-managed --record=record.txt
